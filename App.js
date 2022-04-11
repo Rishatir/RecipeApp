0b6c1389803ae7af184600c9f13ab93696bbba44
Rishatir <dawidwielgosz94@gmail.com>
2022-04-11 19:58:16 +0200

    Add FavButton

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -29,12 +29,12 @@ export default function App() {
           <Stack.Screen
             name="MealsOverview"
             component={MealsOverviewScreen}
-            // options={({ route, navigation }) => {
-             // const catId = route.params.categoryId;
-             // return {
-               // title: catId,
-              // };
-           // }}
+          // options={({ route, navigation }) => {
+          // const catId = route.params.categoryId;
+          // return {
+          // title: catId,
+          // };
+          // }}
           />
           <Stack.Screen name="MealDetail" component={MealDetailScreen} />
         </Stack.Navigator>
